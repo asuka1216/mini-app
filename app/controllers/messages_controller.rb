1c78de1ff6df18e94d5cb4d77bb6ff860178ba2b
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2019-11-09 18:14:27 +0900

    Halfway

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,24 @@
 class MessagesController < ApplicationController
+  
+  before_action :move_to_index, except: :index
 
   def index
-    @messages = Message.all
+    @messages = Message.page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
   end
 
   def create
-    Message.create(message_params)
+    Message.create( text: message_params[:text], user_id: current_user.id)
   end
 
   private
   def message_params
-    params.permit(:text)
+    params.permit(:text, :user_id)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'messages#index'
   get 'messages' => 'messages#index'
   get 'messages/new' => 'messages#new'
   post 'messages' => 'messages#create'
+  get   'users/:id'   =>  'users#show'
 end
