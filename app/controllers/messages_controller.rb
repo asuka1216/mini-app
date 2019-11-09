f4bb0860b7bd6be3f96a7e62454e8497996f3062
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2019-11-09 22:49:12 +0900

    2nd Halfway

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
-    @messages = Message.page(params[:page]).per(5).order("created_at DESC")
+    @messages = Message.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
@@ -11,6 +11,22 @@ class MessagesController < ApplicationController
 
   def create
     Message.create( text: message_params[:text], user_id: current_user.id)
+  end
+
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy if message.user_id == current_user.id
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    message = Message.find(params[:id])
+    if message.user_id == current_user.id
+      message.update(message_params)
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'messages' => 'messages#index'
   get 'messages/new' => 'messages#new'
   post 'messages' => 'messages#create'
+  delete 'messages/:id' => 'messages#destroy'
+  patch 'messages/:id' => 'messages#update'
+  get 'messages/:id/edit' => 'messages#edit'
   get   'users/:id'   =>  'users#show'
 end
